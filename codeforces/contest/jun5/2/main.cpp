e19d07b975831998de01f47e3185f983359e3336
sambey04 <samarthh44@gmail.com>
2019-06-07 06:48:16 +0530

    time limit exceeded

--- codeforces/contest/jun5/2/main.cpp
+++ codeforces/contest/jun5/2/main.cpp
@@ -1,1 +1,17 @@
-#include<>
+#include<bits/stdc++.h>
+using namespace std;
+#define fio      cin.sync_with_stdio(false) ; cin.tie(0);cout.tie(0) ;
+
+int32_t main()
+{
+    fio;
+
+
+
+
+
+    return 0;
+}
+
+
+
--- codeforces/contest/jun5/main.c
+++ codeforces/contest/jun5/main.c
@@ -8,31 +8,41 @@ int32_t main()
     int t;
     cin >> t;
     long long n[19];
-    int k[19];
-   int count=0,a=0;
+   long long k[19];
+
+  long long count=0,a=0;
+
     for( a;a<t;a++)
     {
     cin >> n[a] >> k[a];
-    cout << "in ram";
+    //cout << "in ram";
     }
 
-cout << "ram \n";
+//cout << "ram \n";
     for(a=0;a<t;a++)
     {
-    while(n[a]!=0){
-        if(n[a]%k[a]==0)
+    while(n[a]!=0 ){
+        if(n[a]%k[a]==0   )
     {
+    if(n[a]==1 && k[a]==1){
+        n[a]--;
     count++;
+    }
+    else{
+        count++;
     n[a]=n[a]/k[a];
-   // cout << "value when divide "  << n[a] << " count " << count<< "\n"; 
+//cout << "value when divide= "  << n[a] << " count " << count<< "\n"; 
+    }
     }
     else
     {
-        n[a]--;
-    count++;
+    count=count+n[a]%k[a];
+        n[a]=n[a]-n[a]%k[a];
+//cout << "value in else =" << n[a] <<" coutn=="<< count << "\n";
     }}
     cout << count << "\n";
    count=0;
+//cout << "hello";
     }
 
 }
